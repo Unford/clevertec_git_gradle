0976df3c6b366c39a827444578b38759a7b70333
vladimir_feshenko <vladimir0feshenko@gmail.com>
2023-10-06 15:35:00 +0300

    build dependOn removed

--- plugin/src/main/groovy/ru/clevertec/course/git/GitPlugin.groovy
+++ plugin/src/main/groovy/ru/clevertec/course/git/GitPlugin.groovy
@@ -10,18 +10,18 @@ import ru.clevertec.course.git.task.*
 class GitPlugin implements Plugin<Project> {
 
 
-    private static final String DEFAULT_STAGE_BRANCH_NAME = "stage";
-    private static final String DEFAULT_DEV_BRANCH_NAME = "qa";
-    private static final String DEFAULT_QA_BRANCH_NAME = "dev";
+    private static final String DEFAULT_STAGE_BRANCH_NAME = "stage"
+    private static final String DEFAULT_DEV_BRANCH_NAME = "qa"
+    private static final String DEFAULT_QA_BRANCH_NAME = "dev"
 
-    private static final String PLUGIN_GROUP = "git tagger";
+    private static final String PLUGIN_GROUP = "git tagger"
 
-   public static final String BUILD_TAGGER_NAME = 'buildTagger';
-   public static final String CHECK_UNCOMMITTED_TASK = 'checkUncommitted';
-   public static final String CHECK_HEAD_TAGS_TASK = 'checkHeadTags';
-   public static final String ADD_TAG_TASK = 'addTagToHead';
-   public static final String PUSH_HEAD_TASK = 'pushHeadToRemote';
-   public static final String GET_LAST_VERSION_TASK = 'getLastTagVersion';
+   public static final String BUILD_TAGGER_NAME = 'buildTagger'
+   public static final String CHECK_UNCOMMITTED_TASK = 'checkUncommitted'
+   public static final String CHECK_HEAD_TAGS_TASK = 'checkHeadTags'
+   public static final String ADD_TAG_TASK = 'addTagToHead'
+   public static final String PUSH_HEAD_TASK = 'pushHeadToRemote'
+   public static final String GET_LAST_VERSION_TASK = 'getLastTagVersion'
 
 
     @Override
@@ -30,9 +30,8 @@ class GitPlugin implements Plugin<Project> {
         setDefaultValues(extension)
 
         project.tasks.register(BUILD_TAGGER_NAME, ComplexTagTask) {
-            remoteName = extension.getRemoteName().get();
+            remoteName = extension.getRemoteName().get()
             setGroup(PLUGIN_GROUP)
-            dependsOn(JavaBasePlugin.BUILD_TASK_NAME)
         }
 
         project.tasks.register(GET_LAST_VERSION_TASK, GetLastVersionTask) {
@@ -42,7 +41,6 @@ class GitPlugin implements Plugin<Project> {
         def uncommittedTask = project.tasks.register(CHECK_UNCOMMITTED_TASK,
                 HasNoUncommittedTask) {
             setGroup(PLUGIN_GROUP)
-
         }
 
         def headTagsTask = project.tasks.register(CHECK_HEAD_TAGS_TASK, HeadHasTagTask) {
@@ -62,18 +60,18 @@ class GitPlugin implements Plugin<Project> {
                         !headTagsTask.get().getState().skipped &&
                         !headTagsTask.get().getResult()
             }
-            devAndQABranches = extension.getDevAndQABranches().get();
-            masterBranchName = extension.getMasterBranchName().get();
-            stageBranchName = extension.getStageBranchName().get();
+            devAndQABranches = extension.getDevAndQABranches().get()
+            masterBranchName = extension.getMasterBranchName().get()
+            stageBranchName = extension.getStageBranchName().get()
         }
 
         project.tasks.register(PUSH_HEAD_TASK, PushHeadTask) {
             setGroup(PLUGIN_GROUP)
             dependsOn(ADD_TAG_TASK)
             onlyIf {
-                !addTagTask.get().getState().skipped && addTagTask.get().getResult();
+                !addTagTask.get().getState().skipped && addTagTask.get().getResult()
             }
-            remote = extension.getRemoteName().get();
+            remote = extension.getRemoteName().get()
         }
 
 
